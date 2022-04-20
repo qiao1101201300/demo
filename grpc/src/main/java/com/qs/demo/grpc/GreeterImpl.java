dab97f3477c13c418334d165d24d318c94f7728d
qiaoshen <15138817898@163.com>
2022-04-20 17:25:48 +0800

    grpc sendFile

--- grpc/src/main/java/com/qs/demo/grpc/GreeterImpl.java
+++ grpc/src/main/java/com/qs/demo/grpc/GreeterImpl.java
@@ -2,11 +2,57 @@ package com.qs.demo.grpc;
 
 import io.grpc.stub.StreamObserver;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class GreeterImpl extends com.qs.demo.grpc.GreeterGrpc.GreeterImplBase {
     @Override
     public void sayHello(com.qs.demo.grpc.HelloRequest request, StreamObserver<com.qs.demo.grpc.HelloReply> responseObserver) {
         com.qs.demo.grpc.HelloReply reply = com.qs.demo.grpc.HelloReply.newBuilder().setMessage(request.getName()).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<FileInfo> sendFile(StreamObserver<Info> responseObserver) {
+        System.out.println("send File");
+        return new StreamObserver<FileInfo>() {
+
+            File file = null;
+            RandomAccessFile writeFile = null;
+
+            @Override
+            public void onNext(FileInfo fileInfo) {
+                System.out.println(fileInfo.getName());
+                System.out.println(fileInfo.getIndex());
+                try {
+                    file = new File(System.getProperty("user.dir") + File.separator + fileInfo.getName());
+                    writeFile = new RandomAccessFile(file, "rw");
+                    writeFile.seek(fileInfo.getIndex() * fileInfo.getSize());
+                    writeFile.write(fileInfo.getArrs().toByteArray());
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                if (writeFile != null) {
+                    try {
+                        writeFile.close();
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+                responseObserver.onNext(Info.newBuilder().setMsg(file.getAbsolutePath()).build());
+                responseObserver.onCompleted();
+            }
+        };
     }
 }
--- grpc/src/main/java/com/qs/demo/grpc/HelloWorldClient.java
+++ grpc/src/main/java/com/qs/demo/grpc/HelloWorldClient.java
@@ -4,21 +4,27 @@ import cn.hutool.core.io.FileUtil;
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
-import java.nio.charset.Charset;
-import java.util.Base64;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class HelloWorldClient {
     private final ManagedChannel channel;
-    private final com.qs.demo.grpc.GreeterGrpc.GreeterBlockingStub blockingStub;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+    private final GreeterGrpc.GreeterStub greeterStub;
 
     public HelloWorldClient(String host, int port) {
         //usePlaintext表示明文传输，否则需要配置ssl
         //channel  表示通信通道
         this.channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
         //存根
-        this.blockingStub = com.qs.demo.grpc.GreeterGrpc.newBlockingStub(this.channel);
+        this.blockingStub = GreeterGrpc.newBlockingStub(this.channel);
+        this.greeterStub = GreeterGrpc.newStub(this.channel);
     }
 
     public void shutdown() throws InterruptedException {
@@ -26,14 +32,75 @@ public class HelloWorldClient {
     }
 
     public void test(String name) {
-        com.qs.demo.grpc.HelloRequest request = com.qs.demo.grpc.HelloRequest.newBuilder().setName(ByteString.copyFrom(SerializationUtil.serializer(name))).build();
-        com.qs.demo.grpc.HelloReply helloReply = blockingStub.sayHello(request);
-        System.out.println(SerializationUtil.deserializer(helloReply.getMessage().toByteArray(),String.class));
+        HelloRequest request = HelloRequest.newBuilder().setName(ByteString.copyFrom(SerializationUtil.serializer(name))).build();
+        HelloReply helloReply = blockingStub.sayHello(request);
+        System.out.println(SerializationUtil.deserializer(helloReply.getMessage().toByteArray(), String.class));
+    }
+
+    public String sendFile(String filePath) {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            return null;
+        }
+        String value = null;
+        CountDownLatch latch = new CountDownLatch(1);
+        StreamObserver<FileInfo> requestObserver = greeterStub.sendFile(new StreamObserver<Info>() {
+            @Override
+            public void onNext(Info info) {
+                System.out.println(""+info.getMsg());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                latch.countDown();
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                latch.countDown();
+            }
+        });
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+            byte[] buff = new byte[2048];
+            int len;
+            int index = 0;
+            while ((len = is.read(buff)) != -1) {
+                requestObserver.onNext(FileInfo.newBuilder().setName(file.getName()).setIndex(index).setSize(2048L)
+                        .setArrs(ByteString.copyFrom(buff)).build());
+                index++;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            requestObserver.onError(e);
+        } finally {
+            try {
+                assert is != null;
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        requestObserver.onCompleted();
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return value;
     }
 
     public static void main(String[] args) {
         HelloWorldClient client = new HelloWorldClient("127.0.0.1", 50051);
-        String s = FileUtil.readUtf8String("C:\\Users\\qiaoshen\\Desktop\\新建文本文档 (4).txt");
-        client.test(s);
+        client.sendFile("C:\\Users\\qiaoshen\\Desktop\\输入格式-202220419.zip");
+//        client.test("aaa");
+        try {
+            client.shutdown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
