a6d984f6fe9b890ccade324e2530d7e352bd3d39
qiaoshen <1252925124@qq.com>
2022-06-12 16:59:19 +0800

    commit

--- thread/src/main/java/com/qs/demo/thread/DockerUtils.java
+++ thread/src/main/java/com/qs/demo/thread/DockerUtils.java
@@ -210,7 +210,7 @@ public class DockerUtils {
                         "MYSQL_PASSWORD=admin",
                         "REDIS_HOST=192.168.110.21",
                         "REDIS_PORT=6379",
-                        "SPRING_PREFERRED_NETWORK=192.168.110.*"));
+                        "SERVER_IP=192.168.110.21"));
 
         startContainer(container.getId());
 //        removeImage("3210f0c4aaf8");
